d1a039457a0c516a442645cb7ef2e1b12caca9db
Maksim Markevich <markevich.maks2@gmail.com>
2025-03-22 18:50:14 +0300

    Первый коммит проекта

---
+++ src/layouts/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.scss"
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__inner container"></div>
+    </header>
+  )
+}
+
+export default Header
