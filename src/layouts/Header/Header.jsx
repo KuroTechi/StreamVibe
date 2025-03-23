2285c31e706a2a1d1f358b3831bc4bce25e0f23a
Maksim Markevich <markevich.maks2@gmail.com>
2025-03-23 21:22:03 +0300

    Реализован компонент Button, добавлены кнопки поиска и новостей в хедер

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,6 +1,7 @@
 import Logo from "@/components/Logo"
 import "./Header.scss"
 import classNames from "classnames"
+import Button from "@/components/Button"
 
 const Header = (props) => {
   const { url } = props
@@ -44,6 +45,22 @@ const Header = (props) => {
             ))}
           </ul>
         </nav>
+        <div className="header__actions">
+          <Button
+            className="header__button"
+            label="Search"
+            mode="transparent"
+            isLabelHidden
+            iconName="search"
+          />
+          <Button
+            href="/"
+            label="Notifications"
+            mode="transparent"
+            isLabelHidden
+            iconName="notification"
+          />
+        </div>
       </div>
     </header>
   )
